cbdc9b8f56eddf8d2462d5a95fa20fe8f58afb29
tndus628 <leesb062819@gmail.com>
2025-03-11 10:20:56 +0900

    배열 생성 시 주의

--- 1차원 배열/10810.py
+++ 1차원 배열/10810.py
@@ -1,4 +1,9 @@
 n, m = map(int,input().split())
-
-for i in range(m):
+list = [0] * n #리스트에 N개의 0으로 채워진 리스트 만들어져서 특정 인덱스에 값을 바로 할당 가능
+for a in range(m):
     i,j,k = map(int,input().split())
+    for b in range(i,j+1):
+        list[b-1] = k
+
+for a in range(n):
+    print(list[a], end=" ")
