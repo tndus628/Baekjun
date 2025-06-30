fee444c68716afc7d03fb0e447d03975de536fbe
tndus628 <leesb062819@gmail.com>
2025-06-30 17:44:16 +0900

    replace

--- 심화1/2941.py
+++ 심화1/2941.py
@@ -1,12 +1,12 @@
 n = input()
 croatia=['c=','c-','dz=','d-','lj','nj','s=','z=']
 
-for i in croatia:
-    m = n.find(i)
-    if m>=0:
-       n=n.replace(n[m:m+len(i)],"*")
-print(len(n))
-
 # for i in croatia:
-#     n = n.replace(i,"*")
+#     m = n.find(i)
+#     if m>=0:
+#        n=n.replace(n[m:m+len(i)],"*")
 # print(len(n))
+
+for i in croatia:
+    n = n.replace(i,"*")
+print(len(n))
