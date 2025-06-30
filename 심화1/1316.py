3aaa4f0f14e6e3b132096a8197b2e5a444424290
tndus628 <leesb062819@gmail.com>
2025-06-30 20:29:47 +0900

    find

--- 심화1/1316.py
+++ 심화1/1316.py
@@ -1,4 +1,14 @@
 n = int(input())
-
+cnt=n
 for i in range(n):
+    k=0
     m = input()
+    for j in range(len(m)-1):
+        if m[j]==m[j+1]:
+            pass
+        else:
+            if m.find(m[j],j+1,len(m))>0:
+                k=-1
+    if k==-1:
+        cnt-=1
+print(cnt)
